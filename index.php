c8fa420e2310e4124f93f7121610213e042d7ae7
stanley uchenna <okikestan@gmail.com>
2017-08-19 14:16:11 +0100

    my first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+include 'C:\xampp\htdocs\uchenna\connection.php';
+
+$query ='SELECT * FROM people';
+
+$result = mysql_query($query); 
+
+  while($person = mysql_fetch_array($result)){
+  	echo "<h3>". $person['Name']  . "</h3>";
+}
+
