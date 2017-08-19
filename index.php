567b1be11033159bf14fbdd56da003d063933a1f
stanley uchenna <okikestan@gmail.com>
2017-08-19 16:42:59 +0100

    my first commit

--- index.php
+++ index.php
@@ -1,12 +1,43 @@
+
 <?php
-include 'C:\xampp\htdocs\uchenna\connection.php';
 
-$query ='SELECT * FROM people';
+if(isset($_POST['post'])){
+$user_name = "root";
+$password = "";
+$database = "lion";
+$server = "localhost";
+$db_handle = mysqli_connect($server, $user_name, $password,$database );
 
-$result = mysql_query($query); 
 
-  while($person = mysql_fetch_array($result)){
-  	echo "<h3>". $person['Name']  . "</h3>";
-  	hiii;
+if ($db_handle) {
+$SQL = "SELECT * FROM biodata ";
+$result = mysqli_query($db_handle,$SQL);
+$count = mysqli_num_rows($result);
+if ($count > 0 ){
+	while ($biodata = mysqli_fetch_assoc($result)){
+		print_r($biodata);
+		
+	}
 }
 
+}
+else {
+print "shii happens";
+
+}
+
+}
+
+?>
+<html>
+<head>
+	<title>
+		firstcommit
+	</title>
+</head>
+<body>
+	<form action="index.php" method="post">
+	  <button class="btn btn-lg btn-primary btn-block" type="submit" name='post'>click the button</button>
+	  </form>
+	</body>
+</html>
